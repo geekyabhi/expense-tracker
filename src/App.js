cf64597fe0b00edef20e9c454520a7889bb5075c
Abhinav Singh <thakurabhinav17122001@gmail.com>
2021-04-10 13:23:26 +0530

    react started

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
-
+import Header from './components/Header'
+import Balance from './components/Balance'
+import IncomeExpenses from './components/IncomeExpenses'
+import TransactionList from './components/TransactionList'
+import AddTransaction from './components/AddTransaction';
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Header></Header>
+      <div className="container">
+        <Balance></Balance>
+        <IncomeExpenses></IncomeExpenses>
+        <TransactionList></TransactionList>
+        <AddTransaction></AddTransaction>
+      </div>
     </div>
   );
 }
