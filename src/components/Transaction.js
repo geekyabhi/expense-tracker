dca42f72751936dc4dd6207224d2e31e95a2cd1a
Abhinav Singh <thakurabhinav17122001@gmail.com>
2021-04-11 12:32:51 +0530

    Add and remove function applied

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,10 +1,19 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import {GlobalContext} from '../context/GlobalState'
 const Transaction = (props) => {
     const sign = props.transaction.amount < 0 ? '-' : '+'
+    const {deleteTransaction}=useContext(GlobalContext)
     return (
         <div>
-            <li className={props.transaction.amount < 0 ? 'minus' : 'plus'}>{props.transaction.text} <span>{sign}${Math.abs(props.transaction.amount)}</span><button className="delete-btn">x</button></li>
+            <li className={props.transaction.amount < 0 ? 'minus' : 'plus'}>
+                {props.transaction.text} 
+                <span>
+                    {sign}${Math.abs(props.transaction.amount)}
+                </span>
+                <button className="delete-btn" onClick={()=>deleteTransaction(props.transaction.id)}>
+                    x
+                </button>
+            </li>
         </div>
     )
 }
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -2,23 +2,33 @@ import React ,{createContext,useReducer} from 'react'
 import AppReducer from './AppReducer'
 
 const initialState={
-    transactions:[
-        { id: 1, text: 'Flower', amount: -20 },
-        { id: 2, text: 'Salary', amount: 300 },
-        { id: 3, text: 'Book', amount: -10 },
-        { id: 4, text: 'Camera', amount: 150 }
-    ]
+    transactions:[]
 }
 export const GlobalContext=createContext(initialState)
 
 export const GlobalProvider=({children})=>{
     const [state,dispatch]=useReducer(AppReducer,initialState)
 
-    console.log(dispatch)
+    //Action
+    function deleteTransaction(id){
+        dispatch({        
+            type:'DELETE_TRANSACTION',
+            payload:id
+        })
+    }
+
+    function addTransaction(transaction){
+        dispatch({
+            type:'ADD_TRANSACTION',
+            payload:transaction
+        })
+    }
 
     return (
         <GlobalContext.Provider value={{
-            transactions:state.transactions
+            transactions:state.transactions,
+            deleteTransaction,
+            addTransaction
         }}>
             {children}
         </GlobalContext.Provider>
